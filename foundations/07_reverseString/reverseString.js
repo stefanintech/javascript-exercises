a5780ee85fbce06343d1d27e6e0272470831c454
Stefan Taitano <stefantaitano@gmail.com>
2026-04-17 00:12:43 -0500

    js exercise 7 complete

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverse().join("")
 };
+
+/*
+PARAMETER is a string
+RETURN a string in reverse
+reverseString('hello there') // returns 'ereht olleh'
+
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
