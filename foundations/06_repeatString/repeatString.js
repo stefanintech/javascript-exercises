c6bb6744ffe36fe5602daf2a02a299cbf2c8c0c3
Stefan Taitano <stefantaitano@gmail.com>
2026-04-17 00:01:04 -0500

    completed exercise 6 javascript

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,28 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finalString = ""
+    if (num < 0) {
+        return "ERROR"
+    } else {
+        for (let i = 1; i <= num; i++) {
+            finalString += string
+        }
+    }
+    return finalString
 };
+
+// given a string and a number
+// return the string a given number of times
+// 'hey', 3 = heyheyhey. if num is negative, return ERROR
+// IF num < 0, return "ERROR"
+// ELSE
+    // concat the string X amount of times
+
+/*
+for (let i = 1; i <= num; i++) {
+    console.log(num)
+}
+
+*/
 
 // Do not edit below this line
 module.exports = repeatString;
