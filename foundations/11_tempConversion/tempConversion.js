425ee4771412d4c9f3dfad917481097275410d28
Stefan Taitano <stefantaitano@gmail.com>
2026-04-18 11:54:33 -0500

    complete js exercises up to 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c = (f - 32) / (9 / 5)
+  return Math.round(c * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = (c * 1.8) + 32
+  return Math.round(f * 10) / 10
 };
 
 // Do not edit below this line
