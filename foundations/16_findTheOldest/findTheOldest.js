422d31b8091994da2356a22629984626352501ee
Stefan Taitano <stefantaitano@gmail.com>
2026-05-08 23:47:34 -0500

    Complete second half of javascript foundations

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrPeople) {
+  const currentYear = new Date().getFullYear();
+    const oldest = arrPeople.sort((a,b) => {
+        const lastPerson = (!a.yearOfDeath ? currentYear : a.yearOfDeath) - a.yearOfBirth
+        const nextPerson = (!b.yearOfDeath ? currentYear : b.yearOfDeath) - b.yearOfBirth
 
+        return lastPerson > nextPerson ? -1 : 1
+    })
+
+    return oldest[0]
+    
 };
 
 // Do not edit below this line
